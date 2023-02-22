bbf81118077d43ca4b390970eb5de8e715461cfc
MLD4 <dongmiao123@gmail.com>
2023-02-22 21:59:46 +1030

    tests made

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,19 +1,19 @@
 const Manager = require("../lib/Manager");
 
 describe("Manager", () => {
-    test("Test if officeNumber is present", () => {
+    it("Test if officeNumber is present", () => {
         const officeNumber = "5552101"; //i met a man going to st ives
         const testManager = new Manager("John Smith", 12345, "JohnSmith@fakeEmail.com", officeNumber);
         expect(testManager.officeNumber).toBe(officeNumber);
     });
 
-    test("Test getOfficeNumber()", () => {
+    it("Test getOfficeNumber()", () => {
         const officeNumber = "5552101";
         const testManager = new Manager("John Smith", 12345, "JohnSmith@fakeEmail.com", officeNumber);
         expect(testManager.getOfficeNumber()).toBe(officeNumber);
     });
 
-    test("Test getRole()", () => {
+    it("Test getRole()", () => {
         const testManager = new Manager();
         expect(testManager.getRole()).toBe("Manager");
     });
